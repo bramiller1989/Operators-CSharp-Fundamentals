0d07a6d9e4ba5bc4243607bde0369f55d5c7c2d2
bramiller1989 <bramiller1989@gmail.com>
2021-01-18 22:06:19 -0600

    division

--- Program.cs
+++ Program.cs
@@ -9,7 +9,15 @@ namespace Operators
             var a = 10;
             var b = 3;
 
+            Console.WriteLine("Addition: 10 + 3");
             Console.WriteLine(a + b);
+
+            Console.WriteLine("-----------------\n");
+
+            Console.WriteLine("Division: 10 / 3");
+            Console.WriteLine((float)a / (float)b);
+
+
         }
     }
 }
