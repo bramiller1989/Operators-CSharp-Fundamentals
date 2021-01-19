72ce16090c6e08aefcfcdf266c14a9e6b338971e
bramiller1989 <bramiller1989@gmail.com>
2021-01-18 22:01:52 -0600

    addition

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Operators
+﻿using System;
+
+namespace Operators
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var a = 10;
+            var b = 3;
+
+            Console.WriteLine(a + b);
         }
     }
 }
