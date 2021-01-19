4d041504db9b3c69dadf6edded64e02daaf713fd
bramiller1989 <bramiller1989@gmail.com>
2021-01-18 22:14:16 -0600

    less than

--- Program.cs
+++ Program.cs
@@ -38,6 +38,11 @@ namespace Operators
 
             Console.WriteLine("-----------------\n");
 
+            Console.WriteLine("Is 1 less than 2: 1 < 2");
+            Console.WriteLine(a < b);
+
+            Console.WriteLine("-----------------\n");
+
 
         }
     }
